10480b5a7a6abb30c70fbef30cb2ba89ac2af48c
Kenneth SOARES <kensoares@gmail.com>
2024-10-02 10:37:45 +0200

    partie graphique des options

--- src/OptionMenu.cpp
+++ src/OptionMenu.cpp
@@ -8,8 +8,9 @@
 
 enum Option {
     PLAYER_SPEED = 0,
-    BOMB_NB,
+   // BOMB_NB,
     SHOT_TYPE,
+    BACK,
 };
 
 
@@ -23,13 +24,49 @@ OptionMenu::OptionMenu() : BaseScene() {
 OptionMenu::~OptionMenu() = default;
 
 int OptionMenu::update(int _count) {
-    if (IsKeyPressed(KEY_SPACE)) {
+
+    if(IsKeyPressed(KEY_DOWN)) {
+        select++;
+        select = select % (BACK + 1); // wrapping around
+    }
+
+    if(IsKeyPressed(KEY_UP)) {
+        if(select > 0) {
+            select--;
+        } else {
+            select = BACK;
+        }
+    }
+
+    if(select == BACK && IsKeyPressed(KEY_ENTER)) {
         return Ecrans::MENU;
     }
+
     return Ecrans::OPTIONS;
 }
 
 void OptionMenu::draw() {
     ClearBackground(BLACK);
     DrawTextEx(gameFont, "Options", {xCenterText, 100}, gameNameSize, gameFontSpacing, (Color) PURPLE);
+
+    if(select == PLAYER_SPEED) {
+        DrawTextEx(gameFont, "Player Speed: ", {40, 300}, menuItemSize, gameFontSpacing, (Color){153, 230, 0, 255});
+    } else {
+        DrawTextEx(gameFont, "Player Speed: ", {40, 300}, menuItemSize, gameFontSpacing, WHITE);
+    }
+
+    DrawTextEx(gameFont, "Bomb number: ", {40, 400}, menuItemSize, gameFontSpacing, GRAY);
+
+    if(select == SHOT_TYPE) {
+        DrawTextEx(gameFont, "Shot Type: ", {40, 500}, menuItemSize, gameFontSpacing, (Color){153, 230, 0, 255});
+    } else {
+        DrawTextEx(gameFont, "Shot Type: ", {40, 500}, menuItemSize, gameFontSpacing, WHITE);
+    }
+
+
+    if(select == BACK) {
+        DrawTextEx(gameFont, "Back", {40, 600}, menuItemSize, gameFontSpacing, (Color){153, 230, 0, 255});
+    } else {
+        DrawTextEx(gameFont, "Back", {40, 600}, menuItemSize, gameFontSpacing, WHITE);
+    }
 }
